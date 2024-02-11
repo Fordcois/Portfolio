f610ce9ff4000417c7f893d8722216113dad7690
Sam Ford <samueljamesford@googlemail.com>
2024-02-11 19:17:21 +0000

    Adds Smoothness to Gradient

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,29 @@
-import Introduction from "@/components/Introduction";
-import NavBar from "@/components/navbar";
 import Image from "next/image";
-import ProfilePicture from '@/assets/sfprofile.jpg';
+import ProfilePicture from '@/assets/LeftBackGround.png';
+import BackgroundImage from '@/assets/RightBackGround.png';
+import Introduction from "@/components/Introduction";
 
 export default function Home() {
   return (
-    <>
-      <div style={{ display: 'flex' }}>
-      <div style={{ width: '40%', backgroundColor: 'red', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative' }}>
-  
-      <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
-        <Image src={ProfilePicture} layout="fill" objectFit="cover" alt="Samuel Ford - Full Stack Developer" />
+    <div style={{ display: 'flex', height: '100vh' }}>
+      {/* Left div (60% width) */}
+      <div style={{ flex: '0 0 50%', position: 'relative' }}>
+        {/* Profile picture as background */}
+        <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', background: `url(${ProfilePicture.src})`, backgroundSize: 'cover', backgroundPosition: 'right' }}></div>
+        Yo Cont Goes here
       </div>
-</div>
-        <div className='IntroRight' style={{ width: '60%', backgroundColor: '#fbb7b5', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <div style={{backgroundColor:'transparent', height:'100%'}}>
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-              <Introduction/>
-            </div>
-          </div>
+      
+      {/* Right div (40% width) */}
+      <div style={{ flex: '0 0 50%', position: 'relative' }}>
+        {/* Background image */}
+        <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', background: `url(${BackgroundImage.src})`, backgroundSize: 'cover', backgroundPosition: 'left' }}></div>
+        
+        {/* Content centered vertically */}
+        <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <Introduction/>
+
         </div>
       </div>
-    </>
+    </div>
   );
 }
