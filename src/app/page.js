394bb225283e4de96d3580c6b15162dcf2aa833e
Sam Ford <samueljamesford@googlemail.com>
2024-05-14 22:38:16 +0100

    Removes unused CSS

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 'use client'
 import { useRef } from "react";
+import HomePage from "@/components/Homepage";
+import Bio from "@/components/Bio";
+import VisualProjects from "@/components/VisualProjects";
 import NonVisualProjects from "@/components/NoVisualProjects";
 import Contact from "@/components/Contact";
-import HomePage from "@/components/Homepage";
-import VisualProjects from "@/components/VisualProjects";
-import Bio from "@/components/Bio";
 
 export default function Home() {
+
   const contactRef = useRef(null);
   const RefFunction = () => contactRef.current?.scrollIntoView({ behavior: 'smooth' });
   
