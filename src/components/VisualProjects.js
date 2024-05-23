742f998426d200528a4feb13fb78802ed9a151b5
Sam Ford <samueljamesford@googlemail.com>
2024-05-23 19:49:38 +0100

    Adds BoozersWeepers Deployment Link

--- src/components/VisualProjects.js
+++ src/components/VisualProjects.js
@@ -11,7 +11,8 @@ const projects = [
     description:
     "BoozersWeepers is a lifestyle web app designed to facilitate the tracking and settlement of informal pub bets and IOUs among friends. Users can search for friends in real-time, and initiate or respond to wager challenges with clear win conditions and deadlines. Notifications remind users when deadlines approach, and victors receive IOUs for pints, which are managed in a virtual Wallet. The platform also features a global leaderboard highlighting top performers and allows the creation of friend groups to monitor each other's betting activities.",
     techUsed: ["MongoDB", "Express", "React", "NodeJS",'CSS'],
-    github:"https://github.com/Fordcois/BoozersWeepers"
+    github:"https://github.com/Fordcois/BoozersWeepers",
+    website:"https://boozersweepers.onrender.com"
     },
     {
     name: "Acebook",
