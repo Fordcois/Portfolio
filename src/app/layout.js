4cf2b0e69bb8bedcdee7331f24bc6ee4acf68d45
Sam Ford <samueljamesford@googlemail.com>
2024-07-08 20:12:17 +0100

    Updates Metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,12 +7,31 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Samuel Ford: Software Developer",
-  description: "Samuel Ford: Software Developer",
+  description: "Experienced Full Stack Developer specializing in JavaScript, React, and modern web development.",
+  keywords: ['Freelance Developer', 'JavaScript Developer', 'Web Development', 'React Developer', 'Full Stack Developer', 'Frontend Developer', 'Backend Developer'],
+  openGraph: {
+      title: 'Samuel Ford: Software Developer',
+      description: "Experienced Full Stack Developer specializing in JavaScript, React, and modern web development.",
+      url: 'https://www.samueljford.com/',
+      siteName: 'Samuel Ford: Software Developer',
+      images: [
+        {
+          url: '/meta_preview.png', 
+          width: 1200,
+          height: 630,
+        }
+      ],
+      locale: 'en_US',
+      type: 'website',
+    }
+
 };
 
 export default function RootLayout({ children }) {
   return (
+
     <html lang="en">
+
       <body className={inter.className}>{children}
       <Analytics />
       <SpeedInsights />
